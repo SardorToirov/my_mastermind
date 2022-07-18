4aebb9a21d61b4a71ccec8dd5b853722f05ba286
SardorToirov <102356919+SardorToirov@users.noreply.github.com>
2022-07-18 21:23:59 +0500

    Add files via upload

---
+++ My Mastermind/my_mastermind.h
@@ -0,0 +1,8 @@
+#ifndef __MASTERMIND_H_
+#define __MASTERMIND_H_
+int len(char* arr);
+int indexOf(char* arr, char c);
+int is_correct(char* arr);
+void secret_code(char a[]);
+char* mystrcpy(char* destination, char* source);
+#endif
